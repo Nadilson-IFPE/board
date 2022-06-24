35e1fde223370dd71a22efde65d3656f3c7940b4
Nadilson <nadilson@protonmail.com>
2022-06-24 04:36:54 -0300

    Pagina Home estática

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import styles from "../styles/styles.module.scss";
 
@@ -10,16 +11,28 @@ export default function Home() {
       <main className={styles.contentContainer}>
         <img src="/images/board-user.svg" alt="Ferramenta board" />
         <section className={styles.callToAction}>
-          <h1>Uma ferramenta para o seu dia. Escreva, planeje e organize-se.</h1>
+          <h1>
+            Uma ferramenta para o seu dia. Escreva, planeje e organize-se.
+          </h1>
           <p>
             <span>100% gratuito</span> e online.
           </p>
         </section>
 
         <div className={styles.donaters}>
-        <img src="https://avatars.githubusercontent.com/u/11899797?s=96&v=4" alt="Usuário 1" />
+          <img
+            src="https://avatars.githubusercontent.com/u/11899797?s=96&v=4"
+            alt="Usuário 1"
+          />
         </div>
       </main>
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  return {
+    props: {},
+    revalidate: 60 * 60, // Atualiza a cada 60 minutos
+  };
+};
